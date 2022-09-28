7f0d7a2efc2d0e68e905d788e2ec591d1c79f501
FullerD3V <97800761+FullerD3V@users.noreply.github.com>
2022-09-28 19:31:07 +0200

    función agregar personas creada

--- src/gestorbdpersonas/Main/MainFXMLController.java
+++ src/gestorbdpersonas/Main/MainFXMLController.java
@@ -7,14 +7,16 @@ package gestorbdpersonas.Main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 
 /**
  *
@@ -34,16 +36,59 @@ public class MainFXMLController implements Initializable {
     @FXML
     private TableColumn colNombre, colApellidos, colEdad;
     
+    String nombre, apellidos;
+    
+    int edad;
+    
+    private ObservableList<Persona> personas = FXCollections.observableArrayList();
+
     @FXML
-    private void btnAgregarOnAction(){}
+    private void txtNombreKeyReleased(KeyEvent event){
+        nombre = txtNombre.getText();
+    }
+    
+    @FXML
+    private void txtNombreKeyTyped(KeyEvent event){
+        if (!event.getCharacter().matches("[A-z]")) {
+            event.consume();
+        }
+    }
+    
+    @FXML
+    private void txtApellidosKeyReleased(KeyEvent event){
+        apellidos = txtApellidos.getText();
+    }
+    
+    @FXML
+    private void txtEdadKeyReleased(KeyEvent event){
+        edad = Integer.valueOf(txtEdad.getText());
+    }
+    
+    @FXML
+    public void txtEdadKeyTyped(KeyEvent event) {
+        if (!event.getCharacter().matches("[0-9]")) {
+            event.consume();
+        }
+    }
+    
+    @FXML
+    private void btnAgregarOnAction(){
+        Persona p = new Persona(nombre, apellidos, edad);
+        this.personas.add(p);
+        this.tblPersona.setItems(personas);
+    }
+    
     @FXML
     private void btnModificarOnAction(){}
+    
     @FXML
     private void btnEliminarOnAction(){}
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        this.colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+        this.colApellidos.setCellValueFactory(new PropertyValueFactory("apellidos"));
+        this.colEdad.setCellValueFactory(new PropertyValueFactory("edad"));
+    }        
 }
---
+++ src/gestorbdpersonas/Main/Persona.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestorbdpersonas.Main;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author alumno
+ */
+public class Persona {
+    private String Nombre;
+    private String Apellidos;
+    private int Edad;    
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (this.Edad != other.Edad) {
+            return false;
+        }
+        if (!Objects.equals(this.Nombre, other.Nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.Apellidos, other.Apellidos)) {
+            return false;
+        }
+        return true;
+    }
+    
+    public Persona(String Nombre, String Apellidos, int Edad){
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.Edad = Edad;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+    
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+}
